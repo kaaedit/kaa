88d0dcbf5868832cce2e48b7a870dd57674e8f0e
Atsuo Ishimoto <ishimoto@gembook.org>
2013-10-30 19:29:23 +0900

    Show $TERM with version info.

--- kaa/version.py
+++ kaa/version.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 KAA_VERSION = (0, 5, 0)
 
 def version_info():
@@ -9,8 +9,10 @@ All Rights Reserved.
 
 Python version: {python_version}
 Platform: {platform}
+$TERM: {term}
 '''.format(
     major=KAA_VERSION[0], minor=KAA_VERSION[1],
     micro=KAA_VERSION[2],
     python_version=sys.version,
-    platform=sys.platform)
+    platform=sys.platform,
+    term=repr(os.environ.get('TERM', ''))[1:-1])
