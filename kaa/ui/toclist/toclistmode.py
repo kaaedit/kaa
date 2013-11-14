1b4e9c6cb5cddf3634f06947d3a5a934e7c9c5e6
Atsuo Ishimoto <ishimoto@gembook.org>
2013-11-14 16:50:42 +0900

    use alt+P/N to move to another classe.

--- kaa/ui/toclist/toclistmode.py
+++ kaa/ui/toclist/toclistmode.py
@@ -67,11 +67,64 @@ class TOCList(filterlist.FilterListMode):
                 f.append_text('default', ' ')
 
 
+    def sel_next_namespace(self, wnd):
+        if not self.items:
+            newsel = None
+        elif self.cursel is None:
+            newsel = self.items[0]
+        else:
+            try:
+                idx = self.items.index(self.cursel)
+            except ValueError:
+                newsel = self.items[0]
+            else:
+                idx += 1
+                while idx < len(self.items):
+                    newsel = self.items[idx]
+                    if newsel.value.token == 'namespace':
+                        break
+                    idx += 1
+                else:
+                    newsel = self.items[0]
+
+        self.update_sel(wnd, newsel, bottom=True)
+        return newsel
+
+    @command('toclistdlg.prev_namespace')
+    @norec
+    @norerun
+    def sel_prev_namespace(self, wnd):
+        bottom = None
+        if not self.items:
+            newsel = None
+        elif self.cursel is None:
+            newsel = self.items[-1]
+            bottom = True
+        else:
+            newsel = None
+            for i, item in enumerate(self.items):
+                if item is self.cursel:
+                    bottom = True
+                    if newsel:
+                        break
+                    newsel = None
+
+                if item.value.token == 'namespace':
+                    newsel = item
+
+            if not newsel:
+                newsel = self.items[-1]
+                
+        self.update_sel(wnd, newsel, bottom=bottom)
+        return newsel
+        
 
 
 toclistdlg_keys = {
     tab: 'filterlistdlg.next',
     (shift, tab): 'filterlistdlg.prev',
+    (alt, 'p'): 'toclistdlg.prev_namespace',
+    (alt, 'n'): 'toclistdlg.next_namespace',
 }
 
 
@@ -79,6 +132,21 @@ class TOCListInputDlgMode(filterlist.FilterListInputDlgMode):
     def init_keybind(self):
         super().init_keybind()
         self.keybind.add_keybind(toclistdlg_keys)
+
+
+    @command('toclistdlg.next_namespace')
+    @norec
+    @norerun
+    def sel_next_namespace(self, wnd):
+        filterlist = wnd.get_label('filterlist')
+        filterlist.document.mode.sel_next_namespace(filterlist)
+
+    @command('toclistdlg.prev_namespace')
+    @norec
+    @norerun
+    def sel_prev_namespace(self, wnd):
+        filterlist = wnd.get_label('filterlist')
+        filterlist.document.mode.sel_prev_namespace(filterlist)
 
 def show_toclist(wnd, toclist):
     def callback(result):
