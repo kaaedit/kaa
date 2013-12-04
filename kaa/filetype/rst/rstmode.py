a8378e63092b13c8fb34bac73d80636217764537
Atsuo Ishimoto <ishimoto@gembook.org>
2013-12-05 01:12:13 +0900

    Rst mode: Recognize a non-ASCII punctuation in unicode as separater.

--- kaa/filetype/rst/rstmode.py
+++ kaa/filetype/rst/rstmode.py
@@ -1,3 +1,4 @@
+import unicodedata
 import copy
 from collections import namedtuple
 from kaa.filetype.default import defaultmode
@@ -24,10 +25,14 @@ RstThemes = {
 }
 
 
+SEP_CHARS = ''.join(
+    c for c in (chr(i) for i in range(0x12000))
+        if unicodedata.category(c) in {'Pd', 'Po', 'Ps', 'Pi', 'Pf'} )
+
 class RstInline(Span):
     WS = ' \t\r\n'
-    STARTS = '\'"([{<' + WS
-    ENDS = '\'".,:;!?-)]}/\\>' + WS
+    STARTS = '\'"([{<' + WS+SEP_CHARS
+    ENDS = '\'".,:;!?-)]}/\\>' + WS+SEP_CHARS
 
     def on_start(self, tokenizer, doc, pos, match):
         if ((pos and (doc.gettext(pos - 1, pos) not in self.STARTS)) or
