15357d6265517257ac5be5fb9dc813ca6d7b8eb2
Atsuo Ishimoto <ishimoto@gembook.org>
2013-12-21 13:32:31 +0900

    Update python console

--- kaa/ui/pyconsole/pythonconsolemode.py
+++ kaa/ui/pyconsole/pythonconsolemode.py
@@ -1,3 +1,4 @@
+import code
 import sys
 import traceback
 from contextlib import contextmanager
@@ -8,19 +9,26 @@ from kaa.filetype.default import defaultmode
 from kaa.filetype.python import pythonmode
 from kaa.command import command, Commands, norec, norerun
 from kaa.theme import Theme, Style
+from kaa.ui.dialog import dialogmode
+from kaa.ui.inputline import inputlinemode
 
 pythonconsole_keys = {
-    (alt, '\r'): ('python.exec'),
-    (alt, '\n'): ('python.exec'),
+    ('\r'): ('python.exec'),
 }
 
 PythonConsoleThemes = {
     'basic': [
-        Style('stdout', 'Default', 'Default'),
+        Style('stdout', 'Orange', 'Default'),
         Style('stderr', 'Magenta', 'Default'),
+        Style('ps', 'Blue', 'Default'),
     ],
 }
 
+class KaaInterpreter(code.InteractiveInterpreter):
+    def runcode(self, code):
+        # send newline before running script
+        sys.stdout.write('\n')
+        super().runcode(code)
 
 class PythonConsoleMode(pythonmode.PythonMode):
     MODENAME = 'Python console'
@@ -34,109 +42,145 @@ class PythonConsoleMode(pythonmode.PythonMode):
         super().init_keybind()
         self.register_keys(self.keybind, self.PYTHONCONSOLE_KEY_BINDS)
 
+    def init_themes(self):
+        super().init_themes()
+        self.themes.append(PythonConsoleThemes)
+
+    def init_tokenizers(self):
+        pass
+
+    def on_set_document(self, document):
+        super().on_set_document(document)
+        self.document.marks['current_script'] = (0, 0)
+        self.interp = KaaInterpreter()
+        
+        self.document.append('>>>', self.get_styleid('ps'))
+        self.document.append('\n', self.get_styleid('default'))
+        p = self.document.endpos()
+        self.document.marks['current_script'] = (p, p)
+
+    def on_add_window(self, wnd):
+        super().on_add_window(wnd)
+
+        cursor = dialogmode.DialogCursor(wnd,
+                     [dialogmode.MarkRange('current_script')])
+        wnd.set_cursor(cursor)
+        f, t = self.document.marks['current_script']
+        wnd.cursor.setpos(f)
+
+    def on_esc_pressed(self, wnd, event):
+        f, t = self.document.marks['current_script']
+        self.document.delete(f, t)
+        wnd.cursor.setpos(f)
+        self.document.undo.clear()
+
     @contextmanager
-    def _redirect_output(self):
+    def _redirect_output(self, wnd):
         stdout = sys.stdout
         stderr = sys.stderr
 
-        output = self.output
-        style_stdout = output.mode.get_styleid('stdout')
-        style_stderr = output.mode.get_styleid('stderr')
+        style_stdout = self.get_styleid('stdout')
+        style_stderr = self.get_styleid('stderr')
 
         class out:
-
-            def __init__(self, style):
+            def __init__(self, document, style):
+                self.document = document
                 self.style = style
 
             def write(self, s):
-                if not output.closed:
-                    output.append(s, self.style)
+                if not self.document.closed:
+                    self.document.append(s, self.style)
 
-        sys.stdout = out(style_stdout)
-        sys.stderr = out(style_stderr)
+        sys.stdout = out(self.document, style_stdout)
+        sys.stderr = out(self.document, style_stderr)
 
         yield
 
         sys.stdout = stdout
         sys.stderr = stderr
 
-        w = output.wnds[0]
-        if not w.closed:
-            # rebuild screen before move cursor
-            w.screen.locate(w.screen.pos, top=True)
-            w.cursor.eof()
-
-    def _eval_str(self, s):
-        try:
-            comp = compile(s, '<interactive input>', 'eval')
-        except SyntaxError:
-            return
-
-        try:
-            import __main__
-            ret = eval(comp, __main__.__dict__, __main__.__dict__)
-            if ret is not None:
-                print(repr(ret))
-        except Exception:
-            traceback.print_exc()
-            return True
-
-        return True
-
-    def _exec_str(self, s):
-        try:
-            comp = compile(s, '<interactive input>', 'exec')
-        except SyntaxError:
-            traceback.print_exc()
-            return
-
-        try:
-            import __main__
-            exec(comp, __main__.__dict__, __main__.__dict__)
-        except Exception:
-            traceback.print_exc()
-            return True
-
-        return True
-
     @command('python.exec')
     @norec
     @norerun
     def exec_script(self, wnd):
-        is_sel = wnd.screen.selection.is_selected()
-        if is_sel:
-            f, t = wnd.screen.selection.get_selrange()
-            s = wnd.document.gettext(f, t)
-        else:
-            s = wnd.document.gettext(0, wnd.document.endpos())
-
-        s = s.rstrip()
-        if not s:
-            return
-
-        scr = s + '\n'
-
-        with self._redirect_output():
-            if not self._eval_str(s):
-                self._exec_str(s)
-
-        if not is_sel:
-            endpos = self.document.endpos()
-            if not self.document.gettext(endpos - 1, endpos).endswith('\n'):
-                self.insert_string(wnd, self.document.endpos(),
-                                                 '\n', update_cursor=False)
-            wnd.screen.selection.set_range(0, endpos)
+        self.document.undo.clear()
+        f, t = self.document.marks['current_script']
+        s = wnd.document.gettext(f, t)
+        with self._redirect_output(wnd):
+            ret = self.interp.runsource(s)
+            if not ret:
+                self.document.append('>>>', self.get_styleid('ps'))
+                self.document.append('\n', self.get_styleid('default'))
+                p = self.document.endpos()
+                self.document.marks['current_script'] = (p, p)
+                wnd.cursor.setpos(p)
+            else:
+                doc = PythonInputlineMode.build(wnd, s)
+                kaa.app.show_dialog(doc)
 
 
-class PythonOutputMode(defaultmode.DefaultMode):
-    MODENAME = 'Python outputs'
+    def exec_str(self, wnd, s):
+        s = s.rstrip()+'\n'
+        f, t = self.document.marks['current_script']
+        self.document.replace(f, t, s, 
+                style=self.get_styleid('default'))
+        self.exec_script(wnd)
+
+inputline_keys = {
+    (alt, '\r'): ('inputline'),
+    (alt, '\n'): ('inputline'),
+}
+
+class PythonInputlineMode(dialogmode.DialogMode, pythonmode.PythonMode):
     DOCUMENT_MODE = False
-    USE_UNDO = False
-    NO_WRAPINDENT = True
+    autoshrink = True
+    USE_UNDO = True
+    auto_indent = True
 
-    def init_themes(self):
-        super().init_themes()
-        self.themes.append(PythonConsoleThemes)
+    PYTHONINPUTLINE_KEY_BINDS = [
+        inputline_keys
+    ]
+
+    def init_keybind(self):
+        super().init_keybind()
+        self.register_keys(self.keybind, self.PYTHONINPUTLINE_KEY_BINDS)
+
+    def init_tokenizers(self):
+        pythonmode.PythonMode.init_tokenizers(self)
+
+    def on_add_window(self, wnd):
+        super().on_add_window(wnd)
+        wnd.cursor.setpos(self.document.endpos())
+
+    def on_esc_pressed(self, wnd, event):
+        # todo: run callback
+        super().on_esc_pressed(wnd, event)
+        popup = wnd.get_label('popup')
+        popup.destroy()
+        kaa.app.messagebar.set_message("Canceled")
+
+    @command('inputline')
+    @norec
+    @norerun
+    def input_line(self, w):
+        s = self.document.gettext(0, self.document.endpos())
+        target = self.target
+
+        # close before execute
+        popup = w.get_label('popup')
+        popup.destroy()
+
+        target.document.mode.exec_str(target, s)
+
+    @classmethod
+    def build(cls, target, s):
+        buf = document.Buffer()
+        doc = document.Document(buf)
+        doc.append(s)
+        mode = cls()
+        doc.setmode(mode)
+        mode.target = target
+        return doc
 
 
 def show_console():
@@ -146,15 +190,5 @@ def show_console():
     cons.setmode(mode)
     cons.set_title('<Python console>')
 
-    buf = document.Buffer()
-    output = document.Document(buf)
-    mode = PythonOutputMode()
-    output.setmode(mode)
-    output.set_title('<Python output>')
-
-    cons.mode.output = output
-
-    frame = kaa.app.show_doc(cons).get_label('frame')
-    frame.splitter.split(vert=False, doc=output)
-
-    kaa.app.messagebar.set_message('Hit alt+Enter to execute script.')
+    kaa.app.show_doc(cons)
+#    kaa.app.messagebar.set_message('Hit alt+Enter to execute script.')
